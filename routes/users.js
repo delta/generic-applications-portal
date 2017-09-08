942f5e7be2e1cc762b1baaab864fb249b9a9bb6c
shivashis <shivashispadhi@gmail.com>
2017-09-08 20:30:55 +0530

    Make tokens random by including current time in the hashed strings

--- routes/users.js
+++ routes/users.js
@@ -35,10 +35,11 @@ router.post('/register', function(req, res) {
       console.log(results)
       passwordHash = md5(password)
       console.log(password, passwordHash)
-      activationToken = md5(emailId)
       date = new Date()
       dateInMs = date.getTime()
+      activationToken = md5(emailId + date)
       activationTokenExpiryTime = new Date(dateInMs + 1800000)
+      console.log(activationTokenExpiryTime) // to debug
       sql = "INSERT INTO user (emailId, name, passwordHash, activationToken, activationTokenExpiryTime) VALUE ?"
       values = [[emailId, name, passwordHash,activationToken, activationTokenExpiryTime]]
       connection.query(sql, [values], function(error, results){
@@ -46,7 +47,7 @@ router.post('/register', function(req, res) {
           console.log(error)
           res.json({status:'500', success:false,message:'Internal Server Error'})
         }else{
-          emailContent = 'http://localhost:3000/users/activate/'+activationToken
+          emailContent = 'http://localhost:3000/users/register/activate/'+activationToken
         //  sendEmail(emailId, emailContent, res, activationToken)
         res.json({status:200, success:true, message:"Please traverse to "+ emailContent})
         }
@@ -56,15 +57,21 @@ router.post('/register', function(req, res) {
 });
 
 router.post('/register/activate/:key', function(req, res, next){
-  connection.query("SELECT emailId FROM user", (error, results)=>{
+  console.log("to complete registration")
+  key = req.params.key
+  connection.query("SELECT * FROM user WHERE activationToken=?",[key], (error, results)=>{
     if(error){
-      console.log(error)
-      res.json({status:500,success:false, message:"Internal Server Error", redirect:'/register/resendToken'} )
+      res.json({status:500, success:false, message: 'Internal Server Error'})
     }else{
-      for (result in results){
-        emailId = results[result].emailId
-        hashedEmail = md5(results[result].emailId)
-        if (hashedEmail === req.params.key){
+      if(results.length){
+        //process activation
+        activationTokenExpiryTime = results[0].activationTokenExpiryTime
+        date = new Date()
+        if(date.getTime() > activationTokenExpiryTime.getTime()){
+          console.log(date.getTime(), activationTokenExpiryTime.getTime())
+          console.log("Got here")
+          res.json({status:200,success:false, message:"Token expired, generate new!", redirect:'/login'})
+        }else{
           var sql = "UPDATE user SET isActive = ?,createdDate = ?, activationToken = ?, activationTokenExpiryTime = ? WHERE emailId = ?"
           var date = new Date()
           connection.query(sql,[true,date, null, null, emailId], (error2, results2)=>{
@@ -78,10 +85,13 @@ router.post('/register/activate/:key', function(req, res, next){
             }
           })
         }
+      }else{
+        res.json({status:200, success:false, message: 'Invalid Token'})
       }
     }
   })
 })
+
 
 //handle forgot password
 router.post('/forgotPassword', function(req, res, next){
@@ -93,9 +103,9 @@ router.post('/forgotPassword', function(req, res, next){
       res.json({status:'500', success:false,message:'Internal Server Error, retry!'})
     }else if(results.length){
       //send password reset token
-      passwordResetToken = md5(emailId+'step') //step is a random word
       date = new Date()
       dateInMs = date.getTime()
+      passwordResetToken = md5(emailId+date)
       passwordResetTokenExpiryTime = new Date(dateInMs + 1800000)
       sql = "UPDATE user SET passwordResetToken = ? , passwordResetTokenExpiryTime = ? WHERE emailId = ? "
       values = [ passwordResetToken, passwordResetTokenExpiryTime, emailId]
@@ -124,57 +134,64 @@ router.post('/forgotPassword/reset/:key', function(req, res, next){
   passwordHash = md5(password)
   verificationToken = req.params.key
   console.log(verificationToken)
-  connection.query('SELECT emailId FROM user', function(error, results){
-    for (result in results){
-      emailId = results[result].emailId
-      freshHashedTokenForVerification = md5(emailId + 'step')
-      if(freshHashedTokenForVerification === verificationToken){
-        console.log('Yayy@')
-        sql = "UPDATE user SET passwordHash = ?, passwordResetToken = ?, passwordResetTokenExpiryTime = ? WHERE emailId = ?"
-        connection.query(sql,[passwordHash, null, null, emailId], function(error, results){
-            if(error){
-              console.log(error)
-              res.json({status:200, message:"Password couldn't be reset try again!"})
-            }else{
+  connection.query('SELECT emailId, passwordResetTokenExpiryTime FROM user WHERE passwordResetToken=?',[verificationToken], function(error, results){
+    if(error){
+      res.json({status:500, success:false, message:'Internal Server Error'})
+    }else{
+      if(results.length){
+        passwordResetTokenExpiryTime = results[0].passwordResetTokenExpiryTime
+        date = new Date()
+        if(date.getTime() >  passwordResetTokenExpiryTime.getTime()){
+          res.json({status:200,success:false, message:"Token expired, generate new!", redirect:'/login'})
+        }else{
+          console.log('Yayy@')
+          sql = "UPDATE user SET passwordHash = ?, passwordResetToken = ?, passwordResetTokenExpiryTime = ? WHERE emailId = ?"
+          connection.query(sql,[passwordHash, null, null, emailId], function(error, results){
+              if(error){
+                console.log(error)
+                res.json({status:200, message:"Password couldn't be reset try again!"})
+              }else{
 
-              //direct to users/dashboard but for now redirect to login
-              res.json({status:200, message:"Password successfully changed!", redirect:'/login'})
-            }
-        })
-        break
+                //direct to users/dashboard but for now redirect to login
+                res.json({status:200, message:"Password successfully changed!", redirect:'/login'})
+              }
+          })
+        }
       }else{
-        console.log(freshHashedTokenForVerification, verificationToken)
-      }
-      if(result == results.length-1){
-        res.json({status:200, success:false, message:"Sorry, you don\'t exist!"})
+        res.json({status:200, success:false, message:'Invalid token!'})
       }
     }
   })
 })
 
 router.post('/register/resendToken', function(req, res, next){
-  emailId = req.body.emailId
-  connection.query("SELECT * FROM user WHERE emailId = ?",[emailId], function(error, results){
-    if(error){
-      console.log(error)
-      res.json({status:500, message:"Internal server error" })
-    }else if(results.length){
-      activationToken = md5(emailId)
-      date = new Date()
-      dateInMs = date.getTime()
-      activationTokenExpiryTime = new Date(dateInMs + 1800000)
-      connection.query("UPDATE user SET(activationToken=?, activationTokenExpiryTime=?) WHERE emailId=?",[activationToken, activationTokenExpiryTime],function(error, results){
-        if(error){
-          res.json({status:500, message:'Try again!'})
-        }else{
-          res.json({status:200, success:true, message:"Please check mail"})
-          //send email(problem)
-        }
-      })
-    }else{
-      res.json({status:200, success:false, message:"Email doesn\'t exist!"})
-    }
-  })
+  if(session.sessId){
+    res.json({status:200, success:false, redirect:'/users/dashboard'})
+  }else{
+    emailId = req.body.emailId
+    connection.query("SELECT * FROM user WHERE emailId = ?",[emailId], function(error, results){
+      if(error){
+        console.log(error)
+        res.json({status:500, message:"Internal server error" })
+      }else if(results.length){
+        date = new Date()
+        dateInMs = Number(date.getTime())
+        activationTokenExpiryTime = new Date(dateInMs + 1800000)
+        activationToken = md5(emailId+date)
+        connection.query("UPDATE user SET activationToken=?, activationTokenExpiryTime=? WHERE emailId=?",[activationToken, activationTokenExpiryTime, emailId],function(error2, results){
+          if(error2){
+            console.log(error2)
+            res.json({status:500, message:'Try again!'})
+          }else{
+            res.json({status:200, success:true, message:"Please check mail", token: activationToken})
+            //send email(problem)
+          }
+        })
+      }else{
+        res.json({status:200, success:false, message:"Email doesn\'t exist!"})
+      }
+    })
+  }
 })
 
 
@@ -182,21 +199,25 @@ router.post('/register/resendToken', function(req, res, next){
 
 //login
 router.post('/login', function(req, res) {
-  emailId = req.body.emailId
-  password = req.body.password
-  passwordHash = md5(password)
-  console.log([emailId, passwordHash, password])
-  connection.query("SELECT * FROM user WHERE (emailId = ?  AND passwordHash = ?)", [emailId, passwordHash], function(error, results, details){
-    if(results){
-      user = results[0]
-      sessId = md5(user.name+user.emailId)
-      session.sessId = sessId
-      console.log(session)
-      res.json({status:200, success:true, message:"Redirect to /dashboard"}) //Give redirection headers
-    }else{
-      res.json({status:200, success:false})
-    }
-  })
+  if(session.sessId){
+    res.json({status:200, success:false, redirect:'/users/dashboard'})
+  }else{
+    emailId = req.body.emailId
+    password = req.body.password
+    passwordHash = md5(password)
+    console.log([emailId, passwordHash, password])
+    connection.query("SELECT * FROM user WHERE (emailId = ?  AND passwordHash = ?)", [emailId, passwordHash], function(error, results, details){
+      if(results){
+        user = results[0]
+        sessId = md5(user.name+user.emailId)
+        session.sessId = sessId
+        console.log(session)
+        res.json({status:200, success:true, message:"Redirect to /dashboard"}) //Give redirection headers
+      }else{
+        res.json({status:200, success:false})
+      }
+    })
+  }
 });
 
 
