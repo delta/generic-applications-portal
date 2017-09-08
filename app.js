0ad0368a003da894686dbefebbfe2453c6f61ec8
shivashis <shivashispadhi@gmail.com>
2017-09-08 15:18:08 +0530

    Implement authentication in app.js

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var secretString = require('./config/session_config.js').secretString
 var routes = require('./routes/index');
 var users = require('./routes/users')
-
+var session = require("express-session")
 var app = express();
+app.use(session({secret:secretString,cookie: { maxAge: 186000000 }}));
+
 var initDb = require("./middlewares/db_init").initDb
+var authenticate = require("./middlewares/authenticate").authenticate
 initDb()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use(authenticate)
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
