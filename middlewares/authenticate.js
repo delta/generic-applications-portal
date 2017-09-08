fdb9969dcd5faf5de903812f2006041ab685767a
shivashis <shivashispadhi@gmail.com>
2017-09-08 15:17:02 +0530

    Add authentication middleware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,12 @@
+module.exports.authenticate = (req, res, next)=>{
+  console.log(req.session)
+  if(req.session.sessId){
+    connection.query("SELECT * FROM user", function(error, users){
+      for (user of users){
+        console.log(user.name, user.emailId)
+      }
+    })
+  }else{
+    res.end('Fuck off!')
+  }
+}
