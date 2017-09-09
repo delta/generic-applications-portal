c485f3352fed85ba0ea271fa4092e4959c05da27
shivashis <shivashispadhi@gmail.com>
2017-09-09 15:23:30 +0530

    Complete auth implementation

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,9 +1,18 @@
+var md5 = require('md5')
 module.exports.authenticate = (req, res, next)=>{
-  console.log(req.session)
+  console.log(req.session, "ap")
   if(req.session.sessId){
     connection.query("SELECT * FROM user", function(error, users){
+      count=0
       for (user of users){
-        console.log(user.name, user.emailId)
+        count++
+        if(md5(user.name + user.emailId) == req.session.sessId){
+          console.log('user found!')
+          return next()
+        }
+        if(count == users.length){
+          res.json({status:200, success:false, redirect:'/login'})      
+        }
       }
     })
   }else{
