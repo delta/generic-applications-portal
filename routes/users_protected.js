a98a34769e97c1824615e9aafb98cf3dd721c24d
shivashis <shivashispadhi@gmail.com>
2017-09-09 15:28:10 +0530

    Check required parameters before processing them

--- routes/users_protected.js
+++ routes/users_protected.js
@@ -3,30 +3,36 @@ var router = express.Router();
 var md5 = require('md5')
 /* Only for logged users*/
 router.post('/changePassword', function(req, res) {
+  //Check for required parameters before doing anything
   newPassword = req.body.newPassword
   emailId = req.body.emailId
   oldPassword = req.body.oldPassword
-  connection.query("SELECT * FROM user WHERE emailId = ?", [emailId], (error, users)=>{
-    if(error){
-      res.json({status:500,success:false, message: "Internal Server error!"})
-    }else{
-      user = users[0]
-      if(md5(oldPassword) === user.passwordHash){
-        connection.query("UPDATE user SET passwordHash=? WHERE emailId = ?",[md5(newPassword), emailId], (error2)=>{
-          if(error2){
-            res.json({status:500,success:false, message: "Internal Server error!"})
-          }else{
-            res.json({status:200,success:true, message: "Password updated!"})
-          }
-        })
+  if(newPassword && emailId && oldPassword){
+    connection.query("SELECT * FROM user WHERE emailId = ?", [emailId], (error, users)=>{
+      if(error){
+        res.json({status:500,success:false, message: "Internal Server error!"})
       }else{
-        res.json({status:200,success:false, message: "Old Password Incorrect"})
+        user = users[0]
+        if(md5(oldPassword) === user.passwordHash){
+          connection.query("UPDATE user SET passwordHash=? WHERE emailId = ?",[md5(newPassword), emailId], (error2)=>{
+            if(error2){
+              res.json({status:500,success:false, message: "Internal Server error!"})
+            }else{
+              res.json({status:200,success:true, message: "Password updated!"})
+            }
+          })
+        }else{
+          res.json({status:200,success:false, message: "Old Password Incorrect"})
+        }
       }
-    }
-  })
+    })
+  }else{
+    res.json({status:200, success:false, message:"Send proper params!"})
+  }
 });
 
 router.get('/dashboard', function(req, res){
+  console.log("came to dashboard generator")
   //render dashboard
   res.json({status:200, success:true, message:'Your dashboard.'})
 })
