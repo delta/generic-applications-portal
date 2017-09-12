91a445b79bfdfddb4b3cc435cb2f10bf63fe2525
shivashis <shivashispadhi@gmail.com>
2017-09-12 22:23:35 +0530

    Use isLoggedIn flag for authenticating

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,20 +1,8 @@
 var md5 = require('md5')
 module.exports.authenticate = (req, res, next)=>{
   console.log(req.session, "ap")
-  if(req.session.sessId){
-    connection.query("SELECT * FROM user", function(error, users){
-      count=0
-      for (user of users){
-        count++
-        if(md5(user.name + user.emailId) == req.session.sessId){
-          console.log('user found!')
-          return next()
-        }
-        if(count == users.length){
-          res.json({status:200, success:false, redirect:'/login'})      
-        }
-      }
-    })
+  if(req.session.isLoggedIn){
+    res.json({status:200, success:true})
   }else{
     res.json({status:200, success:false, redirect:'/login'})
   }
