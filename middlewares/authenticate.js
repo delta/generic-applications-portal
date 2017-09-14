50c502e2e54ecb2d2b9f0c535e8e9d0a5d022491
shivashis <shivashispadhi@gmail.com>
2017-09-14 23:26:33 +0530

    Allow req to flow through when authenticated

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -2,7 +2,7 @@ var md5 = require('md5')
 module.exports.authenticate = (req, res, next)=>{
   console.log(req.session, "ap")
   if(req.session.isLoggedIn){
-    res.json({status:200, success:true})
+    next();
   }else{
     res.json({status:200, success:false, redirect:'/login'})
   }
