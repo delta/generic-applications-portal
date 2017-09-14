d21451086c1c93b3ce0ffe585ea9a3354a434174
shivashis <shivashispadhi@gmail.com>
2017-09-15 00:08:43 +0530

    Switch to bcrypt from md5 in /changePassword route

--- routes/users_protected.js
+++ routes/users_protected.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var md5 = require('md5')
+var bcrypt = require('bcrypt')
 /* Only for logged users*/
+const saltRounds = 10;
 router.post('/changePassword', function(req, res) {
   //Check for required parameters before doing anything
   newPassword = req.body.newPassword
@@ -10,20 +12,27 @@ router.post('/changePassword', function(req, res) {
   if(newPassword && emailId && oldPassword){
     connection.query("SELECT * FROM user WHERE emailId = ?", [emailId], (error, users)=>{
       if(error){
+        console.log(error)
         res.json({status:500,success:false, message: "Internal Server error!"})
       }else{
         user = users[0]
-        if(md5(oldPassword) === user.passwordHash){
-          connection.query("UPDATE user SET passwordHash=? WHERE emailId = ?",[md5(newPassword), emailId], (error2)=>{
-            if(error2){
-              res.json({status:500,success:false, message: "Internal Server error!"})
-            }else{
-              res.json({status:200,success:true, message: "Password updated!"})
-            }
-          })
-        }else{
-          res.json({status:200,success:false, message: "Old Password Incorrect"})
-        }
+        bcrypt.compare(oldPassword+emailId, user.passwordHash, function(err, response){
+          if(response){
+            bcrypt.hash(newPassword+emailId,saltRounds, function(err, hash2){
+              console.log(err)
+              connection.query("UPDATE user SET passwordHash=? WHERE emailId = ?",[hash2, emailId], (error2)=>{
+                if(error2){
+                  console.log(error2);
+                  res.json({status:500,success:false, message: "Internal Server error!"})
+                }else{
+                  res.json({status:200,success:true, message: "Password updated!"})
+                }
+              })
+            })
+          }else{
+            res.json({status:200,success:false, message: "Old Password Incorrect"})
+          }
+        })
       }
     })
   }else{
