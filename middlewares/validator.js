6e0ff7cb14976614bac96ec2def57384f3231526
shivashis <shivashispadhi@gmail.com>
2017-09-21 18:53:50 +0530

    Add and implement npm-validator middleware

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,10 +1,6 @@
-module.exports.validate = (req, res, next)=>{
-  if(req.emailId){
-    //check emailId
-  }else if(req.password){
-    //check password
-  }else if(req.name){
-    //check name
-  }
-  next()
+const validator = require('validator')
+module.exports.validate = (toBeValidated, type)=>{
+	if(type === 'email'){
+		return validator.isEmail(toBeValidated);
+	}
 }
