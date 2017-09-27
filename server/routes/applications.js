09fda6bc3e6ea749aa7f95c694c594fe4b010f54
Parth Thakkar <thakkarparth007@gmail.com>
2017-09-28 01:35:16 +0530

    Handle array validation while saving

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -50,22 +50,88 @@ router.post("/save/:id/:section", (req, res) => {
       },
     }))
     .then((formElements) => {
+      // Both of these are to be passed to indicative
       const validationRules = {};
-      const data = {}; // to be passed to indicative
+      const data = {};
+
+      const allKeys = Object.keys(req.body);
 
       for (let i = 0; i < formElements.length; i++) {
         const name = formElements[i].name;
 
-        validationRules[name] = formElements[i].validationRules;
+        if (!/__count__$/.test(name)) {
+          validationRules[name] = formElements[i].validationRules;
+          data[name] = req.body[name];
 
-        // fill only that data which is a valid formElement
-        valuesToStore[name] = {
-          "formElementId": formElements[i].id,
-          "applicationId": appId,
-          "value": req.body[name],
-        };
+          // fill only that data which is a valid formElement
+          valuesToStore[name] = {
+            "formElementId": formElements[i].id,
+            "applicationId": appId,
+            "value": req.body[name],
+          };
+        } else {
+          // we're dealing with an array input. Find all fields in req.body
+          // that correspond to this field, and collect them in an array
+          // and pass them to indicative.
 
-        data[name] = req.body[name];
+          // For example, if the field name in formElements is exampleField__count__
+          // and the validation rule is "required|above:1", and the form
+          // supplied has two fields exampleField1 and exampleField3
+
+          // This (1, then 3) could happen since the frontend allows deleting
+          // arbitrary rows from tableInput or box nodes, and therefore only names
+          // the rows in increasing order.
+
+          // While storing these in database as FormValue, we store them as
+          // a JSON array. That is [exampleField1.value, exampleField3.value] etc.
+
+          const actualName = name.replace(/__count__$/, ""); // strip __count__ suffix
+          const relevantKeys = []; // keys in allKeys that are like exampleField*
+          const values = []; // values corresponding to them, but in order.
+
+          // Collect all the relevant keys.
+          for (let j = 0; j < allKeys.length; j++) {
+            if (allKeys[j].indexOf(actualName) === 0) {
+              relevantKeys.push(allKeys[j]);
+            }
+          }
+
+          // reorder the keys so that we can collect the values into an array.
+          relevantKeys.sort((a, b) => {
+            // basically strip "exampleField" from exampleField1 and exampleField3
+            // get 1 and 3, convert as integers, and then compare them.
+            const aNum = parseInt(a.replace(actualName, ""));
+            const bNum = parseInt(b.replace(actualName, ""));
+
+            return aNum - bNum;
+          });
+
+          // Collect all the values in an array. These values are in correct order.
+          for (let j = 0; j < relevantKeys.length; j++) {
+            const key = relevantKeys[j];
+
+            values.push(req.body[key]);
+          }
+
+          // To indicative, we'll basically pass the rule as
+          // { "exampleField.*": "required|alpha" } or whatever
+          // Here "required|alpha" is the rule stored in db for exampleField__count__
+          validationRules[actualName + "*"] = formElements[i].validationRules;
+
+          // And the data field that we pass is an array of the values
+          // that the user has sent. Basically this:
+          // [ exampleField1.value, exampleField3.value ]
+          // We ensure that the relative order of the fields supplied in the form is
+          // maintained. That's why the above sort exists.
+          data[actualName + "*"] = values;
+
+          // fill only that data which is a valid formElement
+          valuesToStore[actualName + "*"] = {
+            "formElementId": formElements[i].id,
+            "applicationId": appId,
+            "value": values,
+          };
+        }
       }
 
       return indicative.validateAll(data, {});// validationRules);
