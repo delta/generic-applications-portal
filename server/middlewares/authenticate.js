5589be81d899ecf7edbd3d6912f039727534750e
shivashis <shivashispadhi@gmail.com>
2017-10-01 12:44:44 +0530

    Redirect any other unauthorized request to login

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -3,6 +3,6 @@ module.exports.authenticate = (req, res, next) => {
   if (req.session.isLoggedIn) {
     next();
   } else {
-    res.json({ "status": 200, "success": false, "redirect": "/login", });
+    res.redirect('/login');
   }
 };
