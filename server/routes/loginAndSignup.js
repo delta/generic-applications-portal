b00535c8a12a52c31b51e70358661908107f8de3
shivashis <shivashispadhi@gmail.com>
2017-10-02 15:07:45 +0530

    Make login, signup ejs files and modify respective route

--- server/routes/loginAndSignup.js
+++ server/routes/loginAndSignup.js
@@ -6,17 +6,24 @@ const router = express.Router();
 // User login+signup handlers
 
 // GET handlers
+router.get('/', (req, res)=>{
+	if(req.session.isLoggedIn){
+		res.redirect('/dashboard');
+	}else{
+		res.redirect('/login');
+	}
+})
 router.get("/login", (req, res) => {
   if (req.session.isLoggedIn) {
     return res.redirect("/");
   }
-  res.redirect("/login.html");
+  res.render("login");
 });
 router.get("/signup", (req, res) => {
   if (req.session.isLoggedIn) {
     return res.redirect("/");
   }
-  res.redirect("/signup.html");
+  res.render("signup");
 });
 
 
